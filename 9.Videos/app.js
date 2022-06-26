cebd82b4f3972d67f92fa8391cd0748a318be50c
Annapoorneswari H.P <annapoorneswarihp15@gmail.com>
2022-06-26 19:10:50 +0530

    added pause play fucntionality, toggling the button functionality

---
+++ 9.Videos/app.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector('.switch-btn')
+const video = document.querySelector('.video-container')
+
+btn.addEventListener('click', function(){
+    //  btn.classList.toggle('slide')
+    //  console.log('hi')
+    if(!btn.classList.contains('slide')){
+        btn.classList.add('slide')
+        video.pause()
+    }else{
+        btn.classList.remove('slide')
+        video.play()
+    }
+})
