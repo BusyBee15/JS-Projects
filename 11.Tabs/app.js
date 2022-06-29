6ff479847dadb5d44cd5324eb7524f37bc1e89e5
Annapoorneswari H.P <annapoorneswarihp15@gmail.com>
2022-06-29 12:06:19 +0530

    tabs project completed

--- 11.Tabs/app.js
+++ 11.Tabs/app.js
@@ -1,25 +1,23 @@
 const btns = document.querySelectorAll('.tab-btn')
+// console.log(btns)
 const about = document.querySelector('.about')
-const articles = document.querySelectorAll(".content")
-
+// console.log(about)
+const articles = document.querySelectorAll('.content')
+// console.log(articles)
 
 about.addEventListener('click', function(e){
-    console.log(e.target.dataset.id)
     const id = e.target.dataset.id
     if(id){
-        //remove active class on all the buttons
         btns.forEach(function(btn){
             btn.classList.remove('active')
         })
-        e.target.classList.add("active")
-        // hide other articles 
-        articles.forEach(function(article){
-            article.classList.remove('active')
-        })
-        const element = document.getElementById(id)
-        element.classList.add('active')
+        e.target.classList.add('active')
 
+       // hide other articles
+       articles.forEach(function(article){
+           article.classList.remove('active')
+       })
+       const element = document.getElementById(id)
+       element.classList.add('active')
     }
 })
-
-
